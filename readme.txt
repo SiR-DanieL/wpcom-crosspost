6c2d79e929efe611e32bf7b5f2f56e36af285099
Nicola Mustone <mustone.nicola@gmail.com>
2016-09-02 15:08:01 +0200

    added oath2 support replacing the text field for the website

--- readme.txt
+++ readme.txt
@@ -15,7 +15,19 @@ WP.com Cross-Post is a free plugin that syncs your WordPress.com blog with your 
 
 It creates posts on your self-hosted WordPress site by fetching them from WordPress.com through the API and publishes them automatically.
 
-**Cron Jobs in your self-hosted WordPress site must be enabled.**
+= Customization =
+
+This plugin is customizable via filters. The available filters are:
+
+* `wpcom_crosspost_author_email`: Pass the email address of the user to use as author of the cross-posts;
+* `wpcom_crosspost_post_data`: Filter the data before to create the cross-post;
+* `wpcom_crosspost_sync_frequency`: Change the frequency of the scheduled event to check for new posts;
+* `wpcom_crosspost_api_call_params`: Filter the data sent to WordPress.com via API to retrieve posts;
+* `wpcom_crosspost_sinc_from`: Change the "from" date to retrieve posts from WordPress.com. Default "yesterday".
+
+= Google Duplicated Content =
+
+This plugin is compatible with Yoast SEO to prevent duplicated content. Make sure to have Yoast SEO installed and active, the plugin will automatically filter the post URL and set the canonical cross-domain URL pointing to its original WordPress.com permalink.
 
 == Installation ==
 
@@ -28,6 +40,14 @@ In the search field type "WP.com Cross-Post" and click Search Plugins. Once youâ
 = Manual installation =
 
 The manual installation method involves downloading the plugin and uploading it to your webserver via your favourite FTP application. The WordPress codex contains [instructions on how to do this here](https://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+= Configuration =
+
+Go to **Settings > WP.com X-Post** and fill in the Client ID and Secret. If you don't have them, click on the button "Create WordPress.com App" to create an app on WordPress.com.
+
+Save the changes. The button "Connect to WordPress.com" will appear. Click on it to connect your self-hosted site to WordPress.com and choose the blog you want to sync with.
+
+One hour after the first activation there will be the first sync.
 
 == Changelog ==
 
--- wpcom-crosspost.php
+++ wpcom-crosspost.php
@@ -18,17 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class WPCOM_CrossPost {
 	/**
-	 * Plugin version
-	 */
-	const VERSION = '1.0.0';
-
-	/**
-	 * WordPress.com API endpoint.
-	 * @var string
-	 */
-	public $api_url = 'https://public-api.wordpress.com/rest/v1.1/sites/';
-
-	/**
 	 * Plugin settings
 	 * @var object
 	 * @access private
@@ -36,9 +25,33 @@ class WPCOM_CrossPost {
 	private $_settings = null;
 
 	/**
+	 * WPCOM_CrossPost_API instance
+	 * @var WPCOM_CrossPost_API
+	 * @access private
+	 */
+	private $_api = null;
+
+	/**
 	 * Inits the plugin and schedule the hooks.
 	 */
 	public function __construct() {
+		define( 'WPCOM_CROSSPOST_VERSION', '1.0.0' );
+
+		// Include API class
+		require_once __DIR__ . '/class-wpcom-crosspost-api.php';
+
+		// Include admin class
+		if ( is_admin() ) {
+			require_once __DIR__ . '/class-wpcom-crosspost-admin.php';
+		}
+
+		// Load settings
+		$this->_settings = get_option( 'wpcom-crosspost-settings' );
+		$this->_api      = WPCOM_CrossPost_API::instance();
+
+		// Load localization files
+		$this->load_textdomain();
+
 		// Create and clear the schedule for creating cross-posts automatically every day
 		register_activation_hook( __FILE__, array( $this, 'schedule_cross_posts_creation' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'remove_schedule_cross_posts_creation' ) );
@@ -46,14 +59,8 @@ class WPCOM_CrossPost {
 		// Hook the creation process to the scheduled hook
 		add_action( 'wpcom_crossposts_create_posts', array( $this, 'create_cross_posts' ) );
 
-		if ( is_admin() ) {
-			require_once untrailingslashit( __DIR__ ) . '/class-wpcom-crosspost-admin.php';
-		}
-
-		$this->_settings = get_option( 'wpcom-crosspost-settings' );
-
-		// Load localization files
-		$this->load_textdomain();
+		// Sets the cross-domain canonical URL on Yoast SEO
+		add_filter( 'wpseo_canonical', array( $this, 'set_crossdomain_canonical_url' ) );
 	}
 
 	/**
@@ -61,7 +68,7 @@ class WPCOM_CrossPost {
 	 */
 	public function schedule_cross_posts_creation() {
 		if ( ! wp_next_scheduled ( 'wpcom_crossposts_create_posts' ) ) {
-			wp_schedule_event( current_time( 'timestamp' ), apply_filters( 'wpcom_crosspost_sync_frequency', 'daily' ), 'wpcom_crossposts_create_posts' );
+			wp_schedule_event( current_time( 'timestamp' ) + HOUR_IN_SECONDS, apply_filters( 'wpcom_crosspost_sync_frequency', 'daily' ), 'wpcom_crossposts_create_posts' );
 		}
 	}
 
@@ -69,7 +76,11 @@ class WPCOM_CrossPost {
 	 * Clears the scheduled hook whenthe plugin is deactivated.
 	 */
 	public function remove_schedule_cross_posts_creation() {
+		// Clear scheduled events
 		wp_clear_scheduled_hook( 'wpcom_crossposts_create_posts' );
+
+		// Clear options
+		delete_option( 'wpcom-crosspost-settings', $this->_settings );
 	}
 
 	/**
@@ -85,19 +96,24 @@ class WPCOM_CrossPost {
 			$from = absint( $from );
 		}
 
-		return $this->_make_api_call( '/posts', array(
-			'after' => date( 'Y-m-d', $from ),
-		) );
+		$from = apply_filters( 'wpcom_crosspost_sinc_from', $from );
+
+		$response = $this->_api->get_posts( $this->_settings['blog_id'], $from );
+
+		if ( false !== $response && isset( $response->posts ) ) {
+			return $response->posts;
+		}
+
+		return false;
 	}
 
 	/**
 	 * Creates cross-posts from WP.com posts.
 	 *
-	 * @param  string  $from
 	 * @return bool
 	 */
-	public function create_cross_posts( $from = 'yesterday' ) {
-		$posts = $this->get_posts( $from );
+	public function create_cross_posts() {
+		$posts = $this->get_posts( 'yesterday' );
 
 		if ( ! function_exists( 'post_exists' ) ) {
 			require_once( ABSPATH . 'wp-admin/includes/post.php' );
@@ -105,11 +121,7 @@ class WPCOM_CrossPost {
 
 		if ( false !== $posts && count( $posts ) > 0 ) {
 			foreach ( $posts as $post ) {
-				$read_original = apply_filters( 'wpcom_crosspost_more_text', '<p>' . sprintf(
-					__( 'Read more on %s', 'wpcom-crosspost' ),
-					'<a class="more-link" href="' . esc_url( $post->URL ) . '" title="' . sanitize_title( $post->title ) . '">WIHT</a>'
-				) . '</p>', $post );
-
+				// Set author ID to use
 				$author = get_user_by( 'email', apply_filters( 'wpcom_crosspost_author_email', get_bloginfo( 'admin_email' ) ) );
 				if ( ! is_wp_error( $author ) ) {
 					$author = $author->ID;
@@ -117,26 +129,46 @@ class WPCOM_CrossPost {
 					$author = 0;
 				}
 
+				// Create array of tags if any
+				$tags = array();
+				if ( count( $post->tags ) > 0 ) {
+					foreach ( $post->tags as $tag ) {
+						array_push( $tags, esc_html( $tag->name ) );
+					}
+				}
+
+				// Set post data
 				$post_data = apply_filters( 'wpcom_crosspost_post_data', array(
 					'post_date'      => $post->date,
 					'post_title'     => esc_html( $post->title ),
 					'post_name'      => sanitize_title( $post->slug ),
-					'post_content'   => wp_kses_post( $post->excerpt . $read_original ),
+					'post_content'   => wp_kses_post( $post->excerpt ),
 					'comment_status' => $this->_settings['close_comments'] === 'yes' ? 'closed' : 'open',
 					'post_status'    => 'publish',
-					'post_author'    => $author,
-					'post_category'  => array( $this->_settings['category'] ),
+					'post_author'    => absint( $author ),
+					'post_category'  => array( absint( $this->_settings['category'] ) ),
+					'tags_input'     => $tags,
+					'meta_input'     => array(
+						/**
+						 * Support for Yoast SEO's cross-site canonical URL.
+						 *
+						 * @uses wpseo_canonical
+						 * @see  WPCOM_CrossPost::set_crossdomain_canonical_url
+						 */
+						'_wpcom-crosspost-original_url' => esc_url_raw( $post->URL ),
+					)
 				), $post, $this->_settings );
 
+				// If the post does not exist, create it
 				if ( ! post_exists( $post->title, '', $post->date ) ) {
 					$post_id = wp_insert_post( $post_data );
 
+					// Set post format to Link
 					if ( 0 !== $post_id && ! is_wp_error( $post_id ) ) {
 						set_post_format( $post_id, 'link' );
-						update_post_meta( $post_id, '_wpcom-crosspost-original_url', $post->URL );
 					}
 
-					return true;
+					return $post_id;
 				}
 			}
 		}
@@ -145,43 +177,26 @@ class WPCOM_CrossPost {
 	}
 
 	/**
-	 * Loads localization files
+	 * Sets the canonical URL properly with Yoast SEO using the original post URL.
+	 *
+	 * @param  string $url
+	 * @return sttring
 	 */
-	public function load_textdomain() {
-		load_plugin_textdomain( 'wpcom-crosspost', false, untrailingslashit( __DIR__ ) . '/languages' );
+	public function set_crossdomain_canonical_url( $url ) {
+		global $post;
+
+		if ( false !== get_post_meta( $post->ID, '_wpcom-crosspost-original_url', true ) ) {
+			return get_post_meta( $post->ID, '_wpcom-crosspost-original_url', true );
+		}
+
+		return $url;
 	}
 
 	/**
-	 * Makes an API call to WordPress.com
-	 *
-	 * @param  string $endpoint
-	 * @param  array  $params
-	 * @return object|boolean
+	 * Loads localization files
 	 */
-	private function _make_api_call( $endpoint, $params = array() ) {
-		$endpoint = $this->api_url . $this->_settings['website'] . $endpoint;
-
-		$params = apply_filters( 'wpcom_crosspost_api_call_params', wp_parse_args( $params, array(
-			'after'  => date( 'Y-m-d', strtotime( '-1 month' ) ),
-			'order'  => 'ASC',
-			'fields' => 'title,slug,URL,date,excerpt',
-			'status' => 'publish',
-		) ) );
-
-		$query     = http_build_query( $params );
-		$endpoint .= '?'. $query;
-		$data      = array( 'user-agent' => 'WPCOM-CrossPost/' . self::VERSION . ';' . home_url() );
-
-		$response = wp_remote_get( $endpoint, $data );
-
-		error_log( print_r( $response, true ) );
-
-		if ( ! is_wp_error( $response ) && wp_remote_retrieve_response_code( $response ) === 200 ) {
-			$body  = json_decode( wp_remote_retrieve_body( $response ) );
-			return $body->posts;
-		}
-
-		return false;
+	public function load_textdomain() {
+		load_plugin_textdomain( 'wpcom-crosspost', false, __DIR__ . '/languages' );
 	}
 }
 
